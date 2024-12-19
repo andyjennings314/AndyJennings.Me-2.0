69c9793226f0b46b1f4b64c83edad4d71f4bc338
Andy Jennings <andyjennings314@gmail.com>
2024-12-19 13:54:48 +0000

    Pairs Game | Blank component, added routing

---
+++ src/app/components/pairs-game/pairs-game.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pairs-game',
+  imports: [],
+  templateUrl: './pairs-game.component.html',
+  styleUrl: './pairs-game.component.scss'
+})
+export class PairsGameComponent {
+
+}
