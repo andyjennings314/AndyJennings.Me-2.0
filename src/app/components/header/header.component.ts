79bbb18bd0d3a6a63b7271a38c1e517ac7d64ab0
Andy Jennings <andyjennings314@gmail.com>
2024-12-21 22:41:36 +0000

    Adding FA icons

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faEnvelope = faEnvelope;
 }
