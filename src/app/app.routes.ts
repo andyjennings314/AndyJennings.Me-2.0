293bdde6b79b90bcdc45403f0372448f2f2712f1
Andy Jennings <andyjennings314@gmail.com>
2024-12-24 23:00:04 +0000

    Added top-level routes/components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PairsGameComponent } from './components/pairs-game/pairs-game.component';
+import { MyWorkComponent } from './components/my-work/my-work.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'about-me', component: AboutMeComponent },
+    { path: 'my-work', component: MyWorkComponent },
     { path: 'pairs', component: PairsGameComponent },
+    { path: '**', component: HomeComponent }
 ];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
