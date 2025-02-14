ba0f2ce26909614258b8727af1d4693bfdbe9dc2
Andy Jennings <andyjennings314@gmail.com>
2025-02-14 20:56:45 +0000

    FTP GHA | Trying to circumnavigate server caching issues

--- src/app/components/my-work/my-work.component.ts
+++ src/app/components/my-work/my-work.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-my-work',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './my-work.component.html',
   styleUrl: './my-work.component.scss'
 })
